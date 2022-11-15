cff59c8b689b6328c32fe141269be4e2236b1e20
visionlyx <1109220518@qq.com>
2022-11-15 12:24:06 +0800

    Update logger.py

--- segmentation network/utils/logger.py
+++ segmentation network/utils/logger.py
@@ -39,6 +39,5 @@ class Logger2(object):
             tf.summary.scalar(tag, value, step=step)
         self.writer.flush()
 
-
         #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
         #self.writer.add_summary(summary, step)
